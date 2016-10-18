f96b8200884278048a9fca82223be522fd652545
mohong <arthas_qin@qq.com>
2016-10-18 17:40:55 +0800

    即将上映页面解析完成

--- crawler/index.js
+++ crawler/index.js
@@ -12,12 +12,11 @@ const moviedao = require('../server/dao/moviedao');
 
 
 //需要爬取的网页
-
 function getUrl() {
     let urls = [
         "https://movie.douban.com/nowplaying/beijing/",  //正在上映
-       // "https://movie.douban.com/later/beijing/",       //即将上映
-       // "https://movie.douban.com/chart"                 //排行榜
+        "https://movie.douban.com/later/beijing/",        //即将上映
+        //"https://movie.douban.com/chart"                 //排行榜
     ];
     return urls;
 }
@@ -25,10 +24,10 @@ function getUrl() {
 let urls = getUrl();
 
 //控制并发量
-async.eachSeries(urls,(url) => {
+async.forEach(urls,(url) => {
     fetchUrl(url);
-});
-
+    //console.log(url);
+})
 
 //抓取网页内容
 function fetchUrl(url) {
@@ -39,20 +38,28 @@ function fetchUrl(url) {
             'Connection':'keep-alive'
         }
     };
-
     request(options,(error,response,body)=>{
         if (!error && response.statusCode == 200){
-            acquireData(options.url,body);
+            //不同的url选择不同的解析函数
+            switch (options.url){
+                case 'https://movie.douban.com/nowplaying/beijing/':
+                    nowPlayingData(options.url,body);
+                    break;
+                case 'https://movie.douban.com/later/beijing/':
+                    showingSoon(options.url,body);
+                    break;
+                default:
+                    break;
+            }
         }
     })
 }
 
-//解析网页获取数据
-function acquireData(error,data) {
+//解析正在上映页面数据
+function nowPlayingData(error,data) {
     let $ = cheerio.load(data);
     let nowplaying = $('#nowplaying');
     let lisItems = $('.list-item',nowplaying).toArray();
-
     for (let key in lisItems){
         let listItem = lisItems[key];
 
@@ -61,10 +68,33 @@ function acquireData(error,data) {
         let link = $('a',$('.stitle',listItem)).attr('href');
         let rate = $('.subject-rate',$('.srating',listItem)).text();
 
+
+        console.log('nowplaying=========================');
         console.log(post);
         console.log(title);
         console.log(rate);
         console.log(link);
+
     }
 
 }
+
+//解析即将上映页面数据
+function showingSoon(error ,data) {
+    let $ = cheerio.load(data);
+    let divs = $('#showing-soon>div').toArray();
+    for (let key in divs){
+        let div = divs[key];
+
+        let post = $('img',div).attr('src');
+        let title = $('h3>a',div).text();
+        let link = $('h3>a',div).attr('href');
+
+        console.log('showingSoon====================');
+        console.log(post);
+        console.log(title);
+        console.log(link);
+    }
+}
+
+
