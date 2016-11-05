f697966bf1044e1c9a9ab376f9c528f4d84b90c6
mohong <522916411@qq.com>
2016-11-05 22:51:43 +0800

    得到了总页数

--- app/controller/spider.server.controller.js
+++ app/controller/spider.server.controller.js
@@ -28,6 +28,7 @@ module.exports = {
      * 第二次是为了得到每页的20个条目对应的详情url
      */
     getUrlByTag: function (tag) {
+        var total = 0;
         var options = {
             url: 'https://movie.douban.com/tag/'+tag+'?start=20&type=T',
             headers: {
@@ -35,21 +36,32 @@ module.exports = {
                 'Connection':'keep-alive'
             }
         };
+
+        var num = 0;
+        console.log('即将向豆瓣发起请求');
+        var timer = setInterval(function () {
+            options.url = 'https://movie.douban.com/tag/'+tag+'?start='+ num * 20 + '&type=T';
+            getCurUrl(options).then(saveUrl);
+            num ++;
+            console.log('总页数：'+total);
+            if (num == 5){
+                clearInterval(timer);
+            }
+        },2000);
        
         //获取总页数
-        // function getPagesize(options) {
-        //     return new Promise(function (resolve,reject) {
-        //         request(options,function (error,response,body) {
-        //             if (!error && response.statusCode == 200){
-        //                 resolve(parseLink(body).pagesize);
-        //             }
-        //         })
-        //     })
-        // }
-        //
-        // getPagesize(options).then(function (value) {
-        //     console.log('from controller'+value);
-        // });
+        function getPagesize(options) {
+            return new Promise(function (resolve,reject) {
+                request(options,function (error,response,body) {
+                    if (!error && response.statusCode == 200){
+                        resolve(parseLink(body).pagesize);
+                    }
+                })
+            })
+        }
+        getPagesize(options).then(function (value) {
+            total = value;
+        });
 
         //获取当前页的所有url(20个)
         function getCurUrl(options) {
@@ -67,61 +79,11 @@ module.exports = {
         function saveUrl(urls) {
             for (var i=0; i<urls.length; i++){
                 //ParseLinkController.add(url);
+                console.log(urls);
                 if (i=urls.length-1){
                     console.log('已经保存到数据库中！');
                 }
             }
         }
-
-        var num = 0;
-        console.log('即将向豆瓣发起请求');
-        var delay = parseInt((Math.random() * 10000000) % 2000, 10);
-        var timer = setInterval(function () {
-            options.url = 'https://movie.douban.com/tag/'+tag+'?start='+ num * 20 + '&type=T';
-            getCurUrl(options).then(saveUrl);
-            console.log('正在抓取'+options.url+',耗时'+delay+'毫秒');
-            num ++;
-            console.log(parseLink.pagesize);
-            if (num == 5){
-                clearInterval(timer);
-            }
-        },delay);
-
-
-        /*
-        request(options, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var taglink = parseLink(body); //taglink = {'url':url,'pagesize':pagesize}
-                //console.log(body);
-                for(var i=0; i<taglink.pagesize; i++){   //taglink.pagesize
-                    (function (year,i) {
-                        var baseUrl = 'https://movie.douban.com/tag/' + year + '?start='+i*20+'&type=T';
-                        //目的是通过每页的url得到当前页的电影详情url
-
-                        var optionsGetLink = {
-                            url:baseUrl,
-                            headers: {
-                                'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
-                                'Connection':'keep-alive'
-                            }
-                        }
-
-                        request(optionsGetLink, function (error, response, body) {
-                            if (!error && response.statusCode == 200) {
-                                var links = parseLink(body);
-                                for(var key in links.url){
-                                    (function (key) {
-                                        var link = links.url[key];
-                                        console.log(link);
-                                        ParseLinkController.add(link);
-                                    })(key);
-                                }
-                            }
-                        });
-                    })(year,i);
-                }
-            }
-        });
-        */
     }
 };
--- test/testspidercontroller.js
+++ test/testspidercontroller.js
@@ -7,12 +7,5 @@ var SpiderController = require('../app/controller/spider.server.controller');
 var Model = require('../app/models/parselink.server.model');
 var request = require('request');
 
-//SpiderController.getUrlByTag(1988);
-
-var url = 'https://movie.douban.com/tag/2015?start=2800&type=T'
-
-request(url,function (err,body) {
-    if (err) throw err;
-    Model(body);
-    //console.log(body);
-})
+var tag = encodeURI('喜剧');
+SpiderController.getUrlByTag(tag);
