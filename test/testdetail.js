59aac35296e4cda24f6b1ef00dd96e5cbbfb06f2
mohong <522916411@qq.com>
2016-12-11 23:51:45 +0800

    修复部分页面结构不一致导致的报错

--- test/testdetail.js
+++ test/testdetail.js
@@ -2,12 +2,12 @@
  * Created by mohong on 2016/12/11.
  */
 
-var parseMovie = require('./parse_movie');
+var parseMovie = require('../spider/parse_movie');
 var MovieModel = require('../app/models/movie');
 
 var request = require('request');
 
-var pageUrl = 'https://movie.douban.com/subject/25964071/';
+var pageUrl = 'https://movie.douban.com/subject/3266509/';
 getMovieUrl(pageUrl);
 
 function getMovieUrl(pageUrl) {
